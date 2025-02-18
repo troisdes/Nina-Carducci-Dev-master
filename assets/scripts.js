f7541a4df793566b098201567a67fe2f3d869daf
troisdes <117764678+troisdes@users.noreply.github.com>
2025-02-18 08:59:08 +0100

    refactor(scripts): simplify gallery configuration options

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,9 @@
 $(document).ready(function () {
   $(".gallery").mauGallery({
-    columns: {
-      xs: 1,
-      sm: 2,
-      md: 3,
-      lg: 3,
-      xl: 3,
-    },
-    lightBox: true,
+    columns: { xs: 1, sm: 2, md: 3, lg: 3, xl: 3 },
+    lightBox: !0,
     lightboxId: "myAwesomeLightbox",
-    showTags: true,
+    showTags: !0,
     tagsPosition: "top",
   });
 });
